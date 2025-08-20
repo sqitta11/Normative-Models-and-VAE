c84bf413eec31eac55e603747471b436eb85a712
sqitta11 <124260987+sqitta11@users.noreply.github.com>
2025-08-20 18:47:53 +0200

    changed virtual environment

--- normative_models.py
+++ normative_models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = np.sum([1,2,3])
 print(x)
@@ -8,3 +9,9 @@ print('test')
 print("let's figure out how to add files to github")
 
 print('check if it works with github desktop')
+
+x = [1,2,3,4]
+y = x 
+
+plt.plot(x,y)
+plt.show()
